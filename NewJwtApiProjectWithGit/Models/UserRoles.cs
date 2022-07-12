39fe5e8d58c7a0906fa4e63172364ad87c9ed22f
Nikulkin Viacheslav <nikuloid@gmail.com>
2022-07-13 01:47:53 +0300

    create class UserRoles + rebuild the project

---
+++ NewJwtApiProjectWithGit/Models/UserRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace NewJwtApiProjectWithGit.Models
+{
+    public class UserRoles
+    {
+        public string GlobalAdmin { get; set; }
+        public string GlobalUser { get; set; }
+        public string WorkspaceAdmin { get; set; }
+        public string WorkspaceMember { get; set; }
+    }
+}
